7de290b710a49c820fae9ce21960c187e770066e
kameffee <kameffee@gmail.com>
2019-08-30 06:43:08 +0900

    ユーザーの登録処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'signup' => 'users#new'
-
+  
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
-
+  
   get '/' => 'home#top'
-
+  
   get 'about' => 'home#about'
   
+  post 'users/create' => 'users#create'
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
 end
