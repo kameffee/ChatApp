d9025843baa3c9b51bf8a087708ae621800e8e83
kameffee <kameffee@gmail.com>
2019-08-31 07:26:41 +0900

    ユーザー登録時にログイン状態にするように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name: "default_user.jpg")
-    @user.save
+      image_name: "default_user.jpg",
+      password: params[:password])
 
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      # ログイン状態にしておく
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+
   end
 
   def edit
