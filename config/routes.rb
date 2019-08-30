9deca6e9bde985e2022ec48561c3bb6dbc7fef84
kameffee <kameffee@gmail.com>
2019-08-31 07:11:51 +0900

    ログアウト機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
   
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
